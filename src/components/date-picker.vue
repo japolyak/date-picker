32e6be52c9c28fde338c2df39da2c6ff850af8af
japolyak <artemkryvolap@gmail.com>
2023-12-18 18:55:50 +0100

    yeah

---
+++ src/components/date-picker.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <vue-date-picker v-model="date" :enableTimePicker="false" />
+    </div>
+</template>
+
+<script setup lang="ts">
+import VueDatePicker from '@vuepic/vue-datepicker'
+import { ref, watch } from "vue";
+import '@vuepic/vue-datepicker/dist/main.css';
+
+const date = ref();
+
+watch(date, (newVal) => {
+    console.log(newVal);
+});
+</script>
+
+<style scoped>
+
+</style>
