9fa4aa572596ddef99f0b3abc61614010b4adb61
japolyak <artemkryvolap@gmail.com>
2023-12-18 23:58:32 +0100

    feat(main) test

--- src/components/date-picker.vue
+++ src/components/date-picker.vue
@@ -6,10 +6,15 @@
 
 <script setup lang="ts">
 import VueDatePicker from '@vuepic/vue-datepicker'
-import { ref } from "vue";
+import { ref, onMounted } from 'vue';
 import '@vuepic/vue-datepicker/dist/main.css';
 
 const date = ref();
+onMounted(() => {
+    if (window.Telegram.Webapp) {
+        console.log('Telegram Webapp detected');
+    }
+})
 </script>
 
 <style scoped>
