c43054f66cd5bd24e8d4465db23a9726570ea368
japolyak <artemkryvolap@gmail.com>
2023-12-19 00:43:20 +0100

    feat(main) test1

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import App from './App.vue'
 
 const app = createApp(App);
 
-app.config.globalProperties.$telegramWebApp = window.Telegram.WebApp;
+app.config.globalProperties.telegramWebApp = window.Telegram.WebApp;
 
 app.mount('#app')
