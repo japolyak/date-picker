2a41ec5ad0793763c00c869e9567e58e2af8e439
japolyak <artemkryvolap@gmail.com>
2023-12-20 00:26:42 +0100

    test1

--- src/components/date-picker.vue
+++ src/components/date-picker.vue
@@ -5,15 +5,28 @@
         {{ privateCourseId }}
         <v-switch v-model="setAssignment" label="Set assignment" color="primary" hide-details />
         <template v-if="setAssignment">
+
             <v-switch v-model="includeH1" label="Hurra 1" color="primary" hide-details />
-            <v-textarea
-                v-if="includeH1"
-                v-model="setH1"
-                variant="outlined"
-                hide-details
-                auto-grow=""
-                rows="2"
-            />
+            <v-textarea v-if="includeH1" v-model="setH1" variant="outlined" hide-details auto-grow rows="1" />
+
+            <v-switch v-model="includeH2" label="Hurra 2" color="primary" hide-details />
+            <v-textarea v-if="includeH2" v-model="setH2" variant="outlined" hide-details auto-grow rows="1" />
+
+            <v-switch v-model="includeH3" label="Hurra 3" color="primary" hide-details />
+            <v-textarea v-if="includeH3" v-model="setH3" variant="outlined" hide-details auto-grow rows="1" />
+
+            <v-switch v-model="includeCNC" label="Czas na czasownik" color="primary" hide-details />
+            <v-textarea v-if="includeCNC" v-model="setCNC" variant="outlined" hide-details auto-grow rows="1" />
+
+            <v-switch v-model="includeUZ" label="Umiesz zdasz" color="primary" hide-details />
+            <v-textarea v-if="includeUZ" v-model="setUZ" variant="outlined" hide-details auto-grow rows="1" />
+
+            <v-switch v-model="includeMT" label="Megatest" color="primary" hide-details />
+            <v-textarea v-if="includeMT" v-model="setMT" variant="outlined" hide-details auto-grow rows="1" />
+
+            <v-switch v-model="includeAnother" label="Hurra 1" color="primary" hide-details />
+            <v-textarea v-if="includeAnother" v-model="setAnother" variant="outlined" hide-details auto-grow rows="1" />
+
         </template>
     </div>
 </template>
@@ -29,6 +42,18 @@ const date = ref();
 const setAssignment = ref(false);
 const includeH1 = ref(false);
 const setH1 = ref<string | null>(null);
+const includeH2 = ref(false);
+const setH2 = ref<string | null>(null);
+const includeH3 = ref(false);
+const setH3 = ref<string | null>(null);
+const includeCNC = ref(false);
+const setCNC = ref<string | null>(null);
+const includeUZ = ref(false);
+const setUZ = ref<string | null>(null);
+const includeMT = ref(false);
+const setMT = ref<string | null>(null);
+const includeAnother = ref(false);
+const setAnother = ref<string | null>(null);
 
 const route = useRoute();
 const telegramWebApp = inject('telegramWebApp');
