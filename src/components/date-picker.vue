d38f44afda869ad9e238e7132831c2402484f40a
japolyak <artemkryvolap@gmail.com>
2023-12-21 12:44:48 +0100

    test

--- src/components/date-picker.vue
+++ src/components/date-picker.vue
@@ -5,57 +5,41 @@
         <v-switch v-model="setAssignment" label="Set assignment" color="primary" hide-details />
         <template v-if="setAssignment">
             {{ test }}
-            <v-switch v-model="includeH1" label="Hurra 1" color="primary" hide-details />
-            <v-textarea v-if="includeH1" v-model="setH1" variant="outlined" hide-details auto-grow rows="1" />
-
-            <v-switch v-model="includeH2" label="Hurra 2" color="primary" hide-details />
-            <v-textarea v-if="includeH2" v-model="setH2" variant="outlined" hide-details auto-grow rows="1" />
-
-            <v-switch v-model="includeH3" label="Hurra 3" color="primary" hide-details />
-            <v-textarea v-if="includeH3" v-model="setH3" variant="outlined" hide-details auto-grow rows="1" />
-
-            <v-switch v-model="includeCNC" label="Czas na czasownik" color="primary" hide-details />
-            <v-textarea v-if="includeCNC" v-model="setCNC" variant="outlined" hide-details auto-grow rows="1" />
-
-            <v-switch v-model="includeUZ" label="Umiesz zdasz" color="primary" hide-details />
-            <v-textarea v-if="includeUZ" v-model="setUZ" variant="outlined" hide-details auto-grow rows="1" />
-
-            <v-switch v-model="includeMT" label="Megatest" color="primary" hide-details />
-            <v-textarea v-if="includeMT" v-model="setMT" variant="outlined" hide-details auto-grow rows="1" />
-
-            <v-switch v-model="includeAnother" label="Dodatkowe" color="primary" hide-details />
-            <v-textarea v-if="includeAnother" v-model="setAnother" variant="outlined" hide-details auto-grow rows="1" />
-            <v-btn color="primary" @click="planNewClass()">Plan</v-btn>
+            <div v-for="(item, index) in items" :key="index">
+                <v-switch v-model="item.include" :label="item.title" color="primary" hide-details />
+                <v-textarea v-if="item.include" v-model="item.value" variant="outlined" hide-details auto-grow rows="1" />
+            </div>
         </template>
     </div>
 </template>
 
 <script setup lang="ts">
 import VueDatePicker from '@vuepic/vue-datepicker'
-import { ref, onMounted, inject, computed, watch, watchEffect } from 'vue';
+import { ref, onMounted, inject, computed, watchEffect, watch } from 'vue';
 import { useRoute } from 'vue-router';
 import { ApplicationClient } from '@/services/api/application-client';
 import type { SourceDto, NewClassDto } from '@/services/api/api.models';
 
-const date = ref();
+interface Item {
+    title: string;
+    value: string | null;
+    include: boolean;
+}
+
+const date = ref<Date>(null);
 const test = ref('test');
 
 // Assignment
 const setAssignment = ref(false);
-const includeH1 = ref(false);
-const setH1 = ref<string | null>(null);
-const includeH2 = ref(false);
-const setH2 = ref<string | null>(null);
-const includeH3 = ref(false);
-const setH3 = ref<string | null>(null);
-const includeCNC = ref(false);
-const setCNC = ref<string | null>(null);
-const includeUZ = ref(false);
-const setUZ = ref<string | null>(null);
-const includeMT = ref(false);
-const setMT = ref<string | null>(null);
-const includeAnother = ref(false);
-const setAnother = ref<string | null>(null);
+const items = ref<Array<Item>>([
+    { title: 'Hurra 1', value: null, include: false },
+    { title: 'Hurra 2', value: null, include: false },
+    { title: 'Hurra 3', value: null, include: false },
+    { title: 'Czas na czasownik', value: null, include: false },
+    { title: 'Umiesz zdasz', value: null, include: false },
+    { title: 'Megatest', value: null, include: false },
+    { title: 'Dodatkowe', value: null, include: false },
+]);
 
 const route = useRoute();
 const telegramWebApp = inject('telegramWebApp');
@@ -67,34 +51,36 @@ const privateCourseId = computed(() => {
   return isNaN(privateCourseId) ? null : privateCourseId;
 });
 
-const testFn = () => {
-    test.value = 'test2';
-};
-
 watchEffect(() => {
-    telegramWebApp.onEvent('mainButtonClicked', testFn);
+    telegramWebApp.onEvent('mainButtonClicked', planNewClass);
 });
 
 function planNewClass() {
-    if (privateCourseId.value === null) return;
+    test.value = 'test2';
+    if (privateCourseId.value == null || date.value == null || items.value == null) return;
+    const sources: SourceDto[] = items.value.map((item) => {
+        if (item.include && item.value) return { title: item.title, assignment: item.value};
+    });
+
     const a: NewClassDto = {
         date: "asdadsa",
-        sources: [{
-            title: 'asd',
-            assignment: 'asd',
-        }]
+        sources: [...sources]
     };
 
+    test.value = 'testRequest';
     const request = ApplicationClient.planNewClass(privateCourseId.value, a);
 }
 
-onMounted(() => {
-    telegramWebApp.MainButton.text = 'Plan class';
-    telegramWebApp.MainButton.enable();
-    telegramWebApp.MainButton.show();
+watch(date, (newValue, oldValue) => {
+    if (newValue == null && telegramWebApp.MainButton.isVisible) {
+        telegramWebApp.MainButton.disable();
+        telegramWebApp.MainButton.hide();
+    } else if (oldValue == null && !telegramWebApp.MainButton.isVisible) {
+        telegramWebApp.MainButton.text = 'Plan class';
+        telegramWebApp.MainButton.enable();
+        telegramWebApp.MainButton.show();
+    }
 });
-
-watch()
 </script>
 
 <style scoped>
