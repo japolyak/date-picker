ea68e31683a4dd5f1f36eea245490b2d44245cf0
japolyak <artemkryvolap@gmail.com>
2023-12-21 15:50:00 +0100

    test

--- src/components/date-picker.vue
+++ src/components/date-picker.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="px-4 pt-4">
-        <vue-date-picker v-model="date" />
+        <vue-date-picker v-model="date" @update:model-value="setTelegramMainButtonState" />
 
         <div class="d-flex justify-start">
             <v-switch v-model="setAssignment" label="Set assignment" color="primary" hide-details :class="elementTheme" />
@@ -11,7 +11,15 @@
                 {{ test1 }}
             </div>
             <div v-for="(item, index) in items" :key="index">
-                <v-switch v-model="item.include" :label="item.title" color="primary" hide-details :class="elementTheme" />
+                <v-switch
+                    v-model="item.include"
+                    :label="item.title"
+                    color="primary"
+                    hide-details
+                    :class="elementTheme"
+                    :key="`${index}-switch`"
+                    @update:modelValue="!item.include ? item.value = null : null"
+                />
                 <v-textarea
                     v-if="item.include"
                     v-model="item.value"
@@ -20,15 +28,17 @@
                     rows="1"
                     :bg-color="textareaBgColor"
                     auto-grow
+                    :key="`${index}-textarea`"
                 />
             </div>
+            <v-btn text="Test" color="primary" @click="planNewClass" />
         </template>
     </div>
 </template>
 
 <script setup lang="ts">
 import VueDatePicker from '@vuepic/vue-datepicker'
-import { ref, onMounted, inject, computed, watchEffect, watch } from 'vue';
+import { ref, onMounted, inject, computed, watchEffect } from 'vue';
 import { useRoute } from 'vue-router';
 import { ApplicationClient } from '@/services/api/application-client';
 import type { SourceDto, NewClassDto } from '@/services/api/api.models';
@@ -61,8 +71,23 @@ const telegramWebApp = inject('telegramWebApp');
 
 const elementTheme = computed(() => applicationTheme.value === 'dark' ? 'dark-theme' : 'bright-theme');
 const textareaBgColor = computed(() => applicationTheme.value === 'dark' ? '#f1f1f1' : '');
-const telegramMainButtonState = () => {
+const setTelegramMainButtonState = () => {
+    if (date.value && !telegramWebApp.MainButton.isActive) {
+        telegramWebApp.MainButton.text = 'Plan class';
+        telegramWebApp.MainButton.enable();
+        telegramWebApp.MainButton.show();
+        return;
+    }
 
+    if (date.value && !telegramWebApp.MainButton.isVisible) {
+        telegramWebApp.MainButton.show();
+        return;
+    }
+
+    if (!date.value && telegramWebApp.MainButton.isVisible) {
+        telegramWebApp.MainButton.hide();
+        return;
+    }
 };
 
 const privateCourseId = computed(() => {
@@ -77,38 +102,23 @@ watchEffect(() => {
 });
 
 function planNewClass() {
-    console.log('request');
     if (privateCourseId.value == null || date.value == null || items.value == null) return;
-    const sources = items.value.map((item) => {
-        if (item.include && item.value) return { title: item.title, assignment: item.value};
-    });
 
-    const payload = {
-        date: "asdadsa",
+    const sources: SourceDto[] = items.value.map((item) => {
+        if (item.include && item.value) return { title: item.title, assignment: item.value };
+        return null;
+    }).filter((item) => item != null);
+
+    const payload: NewClassDto = {
+        date: "someDate",
         sources: [...sources]
     };
 
-    conosle.log('payload');
-    conosle.log(payload);
     const request = ApplicationClient.planNewClass(privateCourseId.value, payload);
 }
 
-watch(date, (newValue, oldValue) => {
-    if (newValue == null && telegramWebApp.MainButton.isVisible) {
-        telegramWebApp.MainButton.disable();
-        telegramWebApp.MainButton.hide();
-    } else if (oldValue == null && !telegramWebApp.MainButton.isVisible) {
-        telegramWebApp.MainButton.text = 'Plan class';
-        telegramWebApp.MainButton.enable();
-        telegramWebApp.MainButton.show();
-    }
-});
-
 onMounted(() => {
     applicationTheme.value = telegramWebApp.themeParams.secondary_bg_color === '#1c1c1d' ? 'dark' : 'bright';
-    test.value = applicationTheme.value;
-    test1.value = telegramWebApp.themeParams.secondary_bg_color;
-
 });
 </script>
 
